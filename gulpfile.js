5e79ff9191f0af38268baded5137bd62c2ac798d
amaduela <anselmomaduela@gmail.com>
2018-07-29 11:16:01 +0200

    added task for compiling, minify and generate maps for .less files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var sass = require('gulp-less');
+var less = require('gulp-less');
 var concat = require('gulp-concat');
 var cssnano = require('gulp-cssnano');
 var sourcemaps = require('gulp-sourcemaps');
@@ -7,4 +7,13 @@ var sourcemaps = require('gulp-sourcemaps');
 gulp.task('bootstrap', function() {
 	return gulp.src('./node_modules/bootstrap/less/**/**')
 	  .pipe(gulp.dest(('src/less/bootstrap/')));
+});
+
+gulp.task('less', function () {
+	gulp.src('src/less/*.less')
+		.pipe(sourcemaps.init())
+		.pipe(less())
+		.pipe(cssnano())
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('public/css/'));
 });
